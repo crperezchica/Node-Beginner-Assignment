3e89c9c4dd35799738e5b11593429abc7191c231
Carmen Perezchica <crperezchica@gmail.com>
2018-04-15 22:43:00 -0700

    working on non-blocking operators

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
-var http = require("http");
+//Refactored code
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
+var http = require("http");
+var url = require("url");
+
+function start (route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        // console.log("Request received." + pathname + " received.");
+        console.log("Request for " + pathname + "received. ");
+
+
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        var content = route(handle, pathname);
+        response.write("Hello World");
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started."); 
+}
+
+
+
+exports.start = start;
+
+
+
+
+// var http = require("http"); //1 -requires http module that ships with node.js, accessible through the variable http
+
+// http.createServer(function(request, response) { //2-call a function http module offers: createServer.
+//     response.writeHead(200, {"Content-Type": "text/plain"}); //this function returns an object and object has a method named listen, which takes a numerid value indicating port HTTP server is listening
+//     response.write("Hello World");
+//     response.end();
+// }).listen(8888);
